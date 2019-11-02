9b0a4a80a0ff26fb63548782dc14220fa096fe17
lydiali9 <648823000@qq.com>
2019-11-02 10:44:40 +0800

    [feature]: deltool用于在mode=‘production’环境下报错代码行的映射

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,36 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     // 多入口
-    mode: 'development',
+    mode: 'production',
     entry: {
         home: './src/index.js',
-        other: './src/other.js'
     },
     output: {
         // [name]代表这个home 或者other
         filename: "[name].[hash:5].js",
         path: path.resolve(__dirname, 'dist')
     },
+    // 源码映射 会单独生成一个sourcemap文件 出错了会标示当前报错的列和行
+    devtool: "source-map", // 增加映射文件 可以帮我们调试源代码
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            filename: 'home.html',
-            chunks: ['home']
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'other.html',
-            chunks: ['other']
+            filename: 'index.html'
         })
     ]
 }
