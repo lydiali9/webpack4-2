c88800111adfde521cb6922cdb5010ff660cee5c
lydiali9 <648823000@qq.com>
2019-11-02 11:20:36 +0800

    [feature]: devtool source-map定位文件错误的位置

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,14 @@ module.exports = {
         filename: "[name].[hash:5].js",
         path: path.resolve(__dirname, 'dist')
     },
-    // 源码映射 会单独生成一个sourcemap文件 出错了会标示当前报错的列和行
-    devtool: "source-map", // 增加映射文件 可以帮我们调试源代码
+    // 1. 源码映射 会单独生成一个sourcemap文件 出错了会标示当前报错的列和行
+    // devtool: "source-map", // 增加映射文件 可以帮我们调试源代码
+    // 2. b不会产生单独的文件 但是可以显示行和列
+    // devtool: "eval-source-map",
+    // 3. 不会产生列 但是是一个单独的映射文件
+    // devtool: "cheap-module-source-map", // 产生后你可以保留起来
+    // 4. 不会产生文件，集成在打包后的文件中，不会产生列
+    devtool: "cheap-module-eval-source-map",
     module: {
         rules: [
             {
